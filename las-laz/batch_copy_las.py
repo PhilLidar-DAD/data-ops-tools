a22fa3693ec1ff7deae281e277039ae6afbd8bc5
Jok Laurente <jalaurente@dream.upd.edu.ph>
2017-08-14 00:11:46 +0800

    changed algo of the script

--- las-laz/batch_copy_las.py
+++ las-laz/batch_copy_las.py
@@ -1,64 +1,52 @@
-#!/usr/bin/python
-# Salad VM
-
-__version__ = "0.1"
+__version__ = "0.2"
+__author__ = "Jok Laurente"
+__email__ = "jmelaurente@gmail.com"
+__description__ = "Script for batch transferring laz files"
 
 import os
+import csv
 import subprocess
-import argparse
-import time
 
-parser = argparse.ArgumentParser(description='Copy LAS')
-parser.add_argument('-t','--textfile')
-parser.add_argument('-o','--output_folder')
-args = parser.parse_args()
+# parameters
+csv_path = "luzon_area.csv"
+dst_folder = "/mnt/pl2-storage/FREXLS/DATA_FROM_DAD/Extension/_tempLuzon"
+adjusted_las_folder = "/mnt/pmsat-nas_geostorage/DPC/TERRA/Adjusted_LAZ_Tiles"
+las_folder = "/mnt/pmsat-nas_geostorage/DPC/TERRA/LAS_Tiles"
 
-f = open(args.textfile,'r')
-prov = args.textfile.replace('.txt','')
-log = open(prov + '_LAS_log','w')
+with open(csv_path, 'rb') as csvfile:
+	spamreader = csv.reader(csvfile)
+	for row in spamreader:
+		try:
+			print "Finding matches for %s" % row
+			adjusted = False
+			block = str(row).replace("[","").replace("]","").replace("'","")
+			area = block.split("_Blk")[0]
+			wc_block = block + "_20"
 
-orthoDir = f.read()
-lstFolder =str(orthoDir).split("\r\n")
+			print "Finding matches in adjusted folder"
+			for path, dirs, files in os.walk(adjusted_las_folder, topdown=False):
+				for d in dirs:
+					if wc_block in d:
+						adjusted = True
+						src = os.path.join(path,d)
+						print src
+						dst = os.path.join(dst_folder, "Adjusted")
+						cmd = "rsync -aiPS --exclude 'ground' {0} {1}".format(src, dst)
+						subprocess.call(cmd, shell=True)
+						break
 
-print lstFolder,"\n"
-
-# Start timing
-startTime = time.time()
-
-for las in lstFolder:
-	srcLAS = r"/mnt/pmsat-nas_geostorage/DPC/TERRA/Adjusted_LAZ_Tiles/DREAM/"
-	curr = os.path.join(srcLAS,prov,las)
-
-	dst = os.path.join(args.output_folder,prov,las)
-
-	# Check if adjusted
-	if os.path.exists(curr):
-		print "Copying Adjusted LAZ Tiles..."
-		os.makedirs(os.path.join(args.output_folder,'Adjusted',prov,las))
-		dst = os.path.join(args.output_folder,'Adjusted/',prov,las + '/')	
-	else:
-		srcLAS = r"/mnt/pmsat-nas_geostorage/DPC/TERRA/LAS_Tiles/"
-		curr = os.path.join(srcLAS,prov,las,'LAS_FILES/LAZ/')
-		os.makedirs(os.path.join(args.output_folder,prov,las))
-		
-		# Check if LAZ
-		if os.path.exists(curr):
-			print "Copying LAZ Tiles..."
-		else:
-			srcLAS = r"/mnt/pmsat-nas_geostorage/DPC/TERRA/LAS_Tiles/"
-			curr = os.path.join(srcLAS,prov,las,'LAS_FILES/')
-			print "Copying LAS Tiles..."
-
-	for l in os.listdir(curr):
-
-		src = os.path.join(curr,l)
-		
-		rsync = r"rsync -rvtP"
-		fullCmd = ' '.join([rsync, src, dst])
-
-		print fullCmd
-
-		subprocess.call(fullCmd,shell=True,stderr=log)
-
-endTime = time.time()  # End timing
-print '\nElapsed Time:', str("{0:.2f}".format(round(endTime - startTime,2))), 'seconds'
+			print "Finding matches in unadjusted folder"
+			if not adjusted:
+				top_directory = os.path.join(las_folder,area)
+				for path, dirs, files in os.walk(top_directory):
+					for d in dirs:
+						if wc_block in d:
+							src = os.path.join(path,d)
+							print src
+							dst = os.path.join(dst_folder, "Unadjusted")
+							cmd = "rsync -aiPS --exclude 'ASCII' --exclude 'MKP' --exclude '*.las' {0} {1}".format(src, dst)
+							subprocess.call(cmd, shell=True)
+							break
+					break
+		except Exception, e:
+			print "Error in transferring"
